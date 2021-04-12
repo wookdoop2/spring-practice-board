63f0302eb985608d7ef2b9a92871ee5a17b658af
sungwook <steins3391@naver.com>
2021-04-12 21:13:55 +0900

    chapter05 #4 (어노테이션 기반으로 개선하기)

---
+++ src/main/java/com/sungwook/study/springpracticeboard/config/auth/LoginUser.java
@@ -0,0 +1,13 @@
+package com.sungwook.study.springpracticeboard.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// 해당 어노테이션이 생성될 수 있는 위치 지정
+// PARAMETER : 메소드의 파라미터로 선언된 객체에서만 사용할 수 있다.
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
--- src/main/java/com/sungwook/study/springpracticeboard/web/IndexController.java
+++ src/main/java/com/sungwook/study/springpracticeboard/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.sungwook.study.springpracticeboard.web;
 
+import com.sungwook.study.springpracticeboard.config.auth.LoginUser;
 import com.sungwook.study.springpracticeboard.config.auth.dto.SessionUser;
 import com.sungwook.study.springpracticeboard.service.posts.PostsService;
 import com.sungwook.study.springpracticeboard.web.dto.PostsResponseDto;
@@ -16,13 +17,12 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    // private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }

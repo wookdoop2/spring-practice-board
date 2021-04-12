eb5b38503307299f915171b0c10bffe2c4b20e07
sungwook <steins3391@naver.com>
2021-04-12 23:22:04 +0900

    chapter05 #6 (네이버 로그인 연동)

--- src/main/java/com/sungwook/study/springpracticeboard/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/sungwook/study/springpracticeboard/config/auth/dto/OAuthAttributes.java
@@ -27,7 +27,23 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
@@ -49,7 +65,7 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 

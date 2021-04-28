1aeb4b14324f875f57ef45347d01d0982290f50b
sungwook <steins3391@naver.com>
2021-04-28 14:03:30 +0900

    코드 수정 #1

--- src/main/java/com/sungwook/study/springpracticeboard/web/IndexController.java
+++ src/main/java/com/sungwook/study/springpracticeboard/web/IndexController.java
@@ -33,7 +33,7 @@ public class IndexController {
     }
 
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable(value = "id") Long id, Model model) {
+    public String postsUpdate(@PathVariable Long id, Model model) {
 
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
--- src/main/java/com/sungwook/study/springpracticeboard/web/PostsApiController.java
+++ src/main/java/com/sungwook/study/springpracticeboard/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.sungwook.study.springpracticeboard.web;
 
 import com.sungwook.study.springpracticeboard.service.posts.PostsService;
+import com.sungwook.study.springpracticeboard.web.dto.PostsListResponseDto;
 import com.sungwook.study.springpracticeboard.web.dto.PostsResponseDto;
 import com.sungwook.study.springpracticeboard.web.dto.PostsSaveRequestDto;
 import com.sungwook.study.springpracticeboard.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,15 +27,19 @@ public class PostsApiController {
     }
 
     @DeleteMapping("/api/v1/posts/{id}")
-    public Long delete(@PathVariable(value = "id") Long id) {
+    public Long delete(@PathVariable Long id) {
         postsService.delete(id);
-
         return id;
     }
 
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 
 }

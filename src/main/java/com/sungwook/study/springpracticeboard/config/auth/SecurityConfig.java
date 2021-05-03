4497264edcf96010e06be8fd56a34326bf4f4d83
sungwook <steins3391@naver.com>
2021-05-04 02:50:54 +0900

    코드 수정 #8

--- src/main/java/com/sungwook/study/springpracticeboard/config/auth/SecurityConfig.java
+++ src/main/java/com/sungwook/study/springpracticeboard/config/auth/SecurityConfig.java
@@ -21,10 +21,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                     .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**",
-                            "/js/**", "/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
 
                 .and()
                     .logout()
